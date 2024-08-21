0188c2b9b4fbc76ef29562b1d932bf63e0c8ed3c
Abdullah Ayoola <officialayo540@gmail.com>
2024-08-21 18:00:58 +0100

    reduced font size for privacy policy page

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -5,11 +5,13 @@ export default function PrivacyPolicty() {
         <h2 className="text-center text-2xl font-medium xl:text-4xl">
           Privacy Policy
         </h2>
-        <p className="italic">Last Updated: Wednesday Aug 21, 2024</p>
+        <p className="mt-4 text-xs italic lg:text-sm">
+          Last Updated: Wednesday Aug 21, 2024
+        </p>
 
         <div className="mt-4 space-y-4">
           <p className="font-medium">Introduction</p>
-          <p>
+          <p className="text-sm md:text-base">
             Welcome to QuickMnemo. We value your privacy and are committed to
             protecting your personal information. This Privacy Policy explains
             what information we collect, how we use it, and your rights
@@ -17,20 +19,20 @@ export default function PrivacyPolicty() {
           </p>
 
           <p className="text-center font-medium">Information We Collect</p>
-          <p>
+          <p className="text-sm md:text-base">
             <span className="font-medium">Personal Information: </span>
             When you create an account on our website, we collect personal
             information such as your email address.
           </p>
 
           <p className="text-center font-medium">How We Use Your Information</p>
-          <p>
+          <p className="text-sm md:text-base">
             <span className="font-medium">Account Management: </span>
             We use your email to manage your account, provide access to our
             services, and communicate with you regarding your subscription.
           </p>
 
-          <p>
+          <p className="text-sm md:text-base">
             <span className="font-medium">Payment Processing: </span>
             Payments for our services are processed through Flutterwave API. We
             do not store your payment information. Your email may be used to
@@ -40,12 +42,12 @@ export default function PrivacyPolicty() {
           <p className="text-center font-medium">
             Subscription and Cancellation
           </p>
-          <p>
+          <p className="text-sm md:text-base">
             <span className="font-medium">Monthly Subscription: </span>
             Upon successful payment, you are subscribed to a monthly plan that
             grants full access to our mnemonics generation services.
           </p>
-          <p>
+          <p className="text-sm md:text-base">
             <span className="font-medium">Cancellation: </span>
             You can cancel your subscription at any time via your dashboard.
             Upon cancellation, your access to premium features will continue
@@ -53,19 +55,19 @@ export default function PrivacyPolicty() {
           </p>
 
           <p className="text-center font-medium">Data Security</p>
-          <p>
+          <p className="text-sm md:text-base">
             We take reasonable measures to protect your personal information.
             However, please be aware that no method of transmission over the
             internet or method of electronic storage is 100% secure.
           </p>
 
           <p className="text-center font-medium">Your Rights</p>
-          <p>
+          <p className="text-sm md:text-base">
             <span className="font-medium">Access and Update: </span>
             You have the right to access and update your personal information
             through your account settings.
           </p>
-          <p>
+          <p className="text-sm md:text-base">
             <span className="font-medium">Deletion: </span>
             If you wish to delete your account and associated data, you can do
             so via your dashboard or by contacting us directly.
